2cd7f83f4c4d3db16519cf32838c238cd2431ce0
alibenchekroun001 <a.benchekroun@esisa.ac.ma>
2026-07-08 13:25:06 +0100

    backend et frontend pret

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+// Importation des dépendances
+const express = require("express");
+const cors = require("cors");
+
+// Création de l'application Express
+const app = express();
+
+// Port du serveur
+const PORT =  4000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Route test
+app.get("/", (req, res) => {
+  res.send("mon backend de mon jeu uiggherufh ");
+});
+
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
